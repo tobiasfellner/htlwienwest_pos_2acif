e31874c83cb0aa11ee3f188657688f8c3358a637
tobiasfellner <tobias.fellner@me.com>
2026-02-16 17:24:32 +0100

    For Loop

--- 2acif/01_loops/src/Forloop.java
+++ 2acif/01_loops/src/Forloop.java
@@ -1,6 +1,32 @@
+import java.util.Scanner;
+
 public class Forloop {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // i=i+1 == i++ == i+=1
+//        for(int i = 0; i < 10;i+=2){
+//            System.out.println(i);
+//        }
+//
+//        for(int i=100; i >= 0; i--){
+//            System.out.println(i);
+//        }
+
+        System.out.println("======== kleines 1*1");
+        System.out.println("Größe eingeben: ");
+        Scanner scanner = new Scanner(System.in);
+        int upperBound = scanner.nextInt();
+        scanner.close();
+
+        for(int i = 1; i <= upperBound; i++){
+            for(int j = 1; j <= upperBound; j++){
+                System.out.print(j*i);
+                if(j < upperBound){
+                    System.out.print("\t-\t");
+                }
+            }
+            System.out.println();
+        }
+
     }
 
 }
---
+++ 2acif/01_loops/src/While.java
@@ -0,0 +1,14 @@
+public class While {
+    public static void main(String[] args) {
+        // a => a
+        // b => ab
+        // x => abx
+        // 2 => abx und abbrechen
+        char c = '9';
+        boolean isD = Character.isDigit(c);
+
+        while (/*Bedingung*/){
+            // ...
+        }
+    }
+}
