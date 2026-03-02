43733415531c710f8e680843534708a6987d82a8
tobiasfellner <tobias.fellner@me.com>
2026-03-02 19:41:03 +0100

    matrix 2d

--- 2acif/02_arrays/src/Maximum2D.java
+++ 2acif/02_arrays/src/Maximum2D.java
@@ -12,5 +12,22 @@ public class Maximum2D {
 
         // todo Aufgabe: Maximum berechnen und index ausgeben
         // Bsp: Das Maximum liegt auf (1,1) und hat den Wert 1300.
+
+        //             arr[row][col]
+        int maxValue = arr[0][0];
+        int maxRowIndex = 0;
+        int maxColIndex = 0;
+
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                if(arr[row][col] > maxValue){
+                    maxValue = arr[row][col];
+                    maxRowIndex = row;
+                    maxColIndex = col;
+                }
+            }
+        }
+
+        System.out.printf("Das Maximum liegt auf (%d,%d) und hat den Wert %d%n", maxRowIndex, maxColIndex, maxValue);
     }
 }
