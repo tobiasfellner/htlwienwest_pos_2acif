9b91459a645d0493f0b08afde6a2c964a19640a2
tobiasfellner <tobias.fellner@me.com>
2026-04-13 18:41:21 +0200

    Flächenberechnung

--- 2acif/03_methoden/src/AreaCalculation.java
+++ 2acif/03_methoden/src/AreaCalculation.java
@@ -1,11 +1,64 @@
+import java.util.Scanner;
+
 public class AreaCalculation {
+    public static void Main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welche Figur? (rechteck/kreis/dreieck)");
+        String figur = scanner.nextLine();
 
-    public static double umrechnen(double cm){
-        return umrechnen(1);
+        if(figur.equalsIgnoreCase( "rechteck")){
+            System.out.println("Breite eingeben:");
+            double width = Double.parseDouble(scanner.nextLine());
+
+            System.out.println("Höhe eingeben:");
+            double height = scanner.nextDouble();
+            scanner.nextLine();
+
+            double area = calcArea(width, height);
+            System.out.printf("Fläche: %.2f", area);
+        }else if(figur.equalsIgnoreCase("kreis")){
+            System.out.println("Radius eingeben:");
+            double radius = Double.parseDouble(scanner.nextLine());
+            double area = calcArea(radius);
+            System.out.printf("Fläche: %.2f", area);
+        }else if(figur.equalsIgnoreCase("dreieck")){
+            System.out.println("Grundlinie eingeben:");
+            double baseline = Double.parseDouble(scanner.nextLine());
+
+            System.out.println("Höhe eingeben:");
+            double height = scanner.nextDouble();
+            scanner.nextLine();
+
+            double area = calcAreaTriangle(baseline, height);
+            System.out.printf("Fläche: %.2f", area);
+        }else {
+            System.out.println("Falsche Eingabe");
+        }
+        scanner.close();
     }
-    public static void main(String[] args) {
-        System.out.println(umrechnen(1));
+
+    // für compiler:
+    // double calcArea(double, double)
+    public static double calcArea(double width, double height){
+        double result = width * height;
+        return result;
+        //return width * height;
     }
 
 
-}
+    // für compiler:
+    // double calcArea(double)
+    public static double calcArea(double r) {
+        return r*r*Math.PI;
+    }
+
+    public static double calcAreaTriangle(double baseline, double height) {
+        return baseline * height / 2;
+    }
+
+    public static int calcAreaTriangle(int baseline, int height) {
+        return baseline * height / 2;
+    }
+
+
+    }
